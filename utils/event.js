0baf85aa2007e185452619ec0e29464a368028b9
chrisupt2001 <73676459+chrisupt2001@users.noreply.github.com>
2023-01-31 22:52:14 -0500

    v1.1

--- utils/event.js
+++ utils/event.js
@@ -2,20 +2,21 @@ import {add, multiply} from "./math";
 
 const eventHandler = () => {
     const addButton = document.getElementById('add');
+    const multButton = document.getElementById('mult');
     const number1 = document.getElementById('number-1');
     const number2 = document.getElementById('number-2');
     const result = document.getElementById('result');
     
     const updateDomWithAddedNumbers = () => {
-        result.innerHTML = //parseInt(number1.ariaValueMax, 10)+parseInt(number2.ariaValueMax, 10);
-        add(number1.value,number2.value)
-        multiply(number1.value,number2.value)
+        result.innerHTML = add(number1.value,number2.value)
+    }
+    
+    const updateDomWithMultipliedNumbers = () => {
+        result.innerHTML = multiply(number1.value,number2.value)
     }
 
-
-
     addButton.addEventListener('click', updateDomWithAddedNumbers);
-    addButton.addEventListener('click', updateDomWithAddedNumbers);
+    multButton.addEventListener('click', updateDomWithMultipliedNumbers);
 
 }
 
